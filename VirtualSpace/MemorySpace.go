b12cecd955588870d45b9c834a28193e5110c0c8
Senko Minin <swilkfo@gmail.com>
2025-09-08 11:39:49 +0300

    Add ClearPage,Pop and some func

--- VirtualSpace/MemorySpace.go
+++ VirtualSpace/MemorySpace.go
@@ -1,35 +1,49 @@
 package memorySpace
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"syscall"
 )
 
 const (
-	pageSize         = 1024 //1 kb
 	percentRealPages = 0.2
 )
 
+var (
+	memoryLimitError = errors.New("Your size bigger than allocator memory limit")
+	stackOverflow    = errors.New("stackoverflow")
+)
+
 type Allocator struct {
-	RealSize     int
-	VirtualSize  int
+	realSize     int
+	virtualSize  int
 	memoryLimit  int
+	pageSize     int
 	stackPointer int
 	pagesArea    []*Page
 }
+
 type Page struct {
 	data []byte
 }
 
-func NewAllocator(memorySize int) (*Allocator, error) {
+func NewAllocator(memorySize int, memoryLimitSize int, pageSize int) (*Allocator, error) {
+	if memorySize > memoryLimitSize {
+		return nil, memoryLimitError
+	}
+	alloc := &Allocator{
+		memoryLimit:  memoryLimitSize,
+		stackPointer: 0,
+		pageSize:     pageSize,
+		pagesArea:    make([]*Page, 0, 5),
+	}
 	pages := math.Ceil(float64(memorySize) / float64(pageSize))
 	realPages := math.Ceil(pages * percentRealPages)
 	virtualPages := pages - realPages
-	alloc := &Allocator{
-		stackPointer: 0,
-		pagesArea:    make([]*Page, 0, 5),
-	}
+	alloc.realSize = int(realPages) * pageSize
+	alloc.virtualSize = int(virtualPages) * pageSize
 	for i := 0; i < int(realPages); i++ {
 		data, err := syscall.Mmap(-1,
 			0,
@@ -51,16 +65,25 @@ func NewAllocator(memorySize int) (*Allocator, error) {
 
 }
 
+func (a *Allocator) ClearPage(address []byte) error {
+	return syscall.Munmap(address)
+}
+
 func (a *Allocator) Push(value []byte) error {
 	lenghtValue := len(value)
+	pageSize := a.pageSize
 	stackPointer := a.stackPointer
-	pageNumber := stackPointer / pageSize
+	if stackPointer+lenghtValue > a.memoryLimit {
+		return stackOverflow
+	}
+	pageNumber := stackPointer / a.pageSize
 	pageAfterPush := (stackPointer + lenghtValue) / pageSize
 	pageOffset := stackPointer % pageSize
 	deltaPage := pageAfterPush - pageNumber
 
 	for i := 0; i < deltaPage; i++ {
 		page := &Page{}
+		a.realSize += pageSize
 		a.pagesArea = append(a.pagesArea, page)
 	}
 
@@ -87,9 +110,47 @@ func (a *Allocator) Push(value []byte) error {
 	return nil
 }
 
+func (a *Allocator) Pop(valueLenght int) []byte {
+	pageSize := a.pageSize
+	initialStackPointer := a.stackPointer
+	stackPointerAfterPop := initialStackPointer - valueLenght
+	a.stackPointer = stackPointerAfterPop
+	stackPointer := a.stackPointer - 1
+	valueIndex := valueLenght - 1
+	result := make([]byte, valueLenght)
+
+	for i := 0; i < valueLenght; i++ {
+		pageNumber := stackPointer / pageSize
+		pageOffset := stackPointer % pageSize
+		result[valueIndex] = a.pagesArea[pageNumber].data[pageOffset]
+		stackPointer--
+		valueIndex--
+	}
+	initialPage := initialStackPointer / pageSize
+	pageAfterPop := stackPointerAfterPop / pageSize
+
+	for initialPage > pageAfterPop {
+		page := a.pagesArea[initialPage]
+		err := a.ClearPage(page.data)
+		if err != nil {
+			break
+		}
+		a.pagesArea = a.pagesArea[:len(a.pagesArea)-1]
+		initialPage--
+	}
+
+	return result
+}
+
 func (a *Allocator) MOV(offsetValue int, value []byte) error {
+	pageSize := a.pageSize
 	lenghtValue := len(value)
 	offsetAfterMov := offsetValue + lenghtValue
+
+	if offsetAfterMov > a.memoryLimit {
+		return stackOverflow
+	}
+
 	pageNumber := offsetValue / pageSize
 	pageOffset := offsetValue % pageSize
 	pageNumberAfterMov := offsetAfterMov / pageSize
@@ -127,14 +188,23 @@ func (a *Allocator) MOV(offsetValue int, value []byte) error {
 }
 
 func (a *Allocator) GetValue(offset int, lenghtData int) ([]byte, error) {
+	pageSize := a.pageSize
 	if offset+lenghtData > a.stackPointer { //todo: чекнуть условия
-		return nil, fmt.Errorf("offset grather than stackPointer")
+		return nil, fmt.Errorf("offset greater than stackPointer")
 	}
 	result := make([]byte, lenghtData)
 	for i := 0; i < lenghtData; i++ {
-		pageNumber := (offset + i) % pageSize
-		pageOffset := (offset + i) / pageSize
+		pageNumber := (offset + i) / pageSize
+		pageOffset := (offset + i) % pageSize
 		result[i] = a.pagesArea[pageNumber].data[pageOffset]
 	}
 	return result, nil
+}
+
+func (a *Allocator) virtualMemorySize() int {
+	return a.virtualSize
+}
+
+func (a *Allocator) physicalMemorySize() int {
+	return a.realSize
 }
--- test/virtualSpace_test.go
+++ test/virtualSpace_test.go
@@ -3,10 +3,11 @@ package test
 import (
 	memorySpace "MemoryCash_GC/VirtualSpace"
 	"encoding/binary"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"math/rand/v2"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type testCase struct {
@@ -16,8 +17,10 @@ type testCase struct {
 }
 
 const (
-	attempt   = 10
-	spaceSize = 4096
+	attempt     = 10
+	spaceSize   = 4096
+	memoryLimit = 4096 * 2
+	pageSize    = 1024
 )
 
 var (
@@ -25,9 +28,9 @@ var (
 )
 
 func TestVirtualSpaceHappy(t *testing.T) {
-	space, err := memorySpace.NewAllocator(spaceSize)
+	space, err := memorySpace.NewAllocator(spaceSize, memoryLimit, pageSize)
 	require.NoError(t, err)
-	testCases := []testCase{}
+	testCases := make([]testCase, 0, attempt)
 	offset := 0
 	for i := 0; i < attempt; i++ {
 		if i%2 == 0 {
@@ -54,23 +57,56 @@ func TestVirtualSpaceHappy(t *testing.T) {
 		}
 
 	}
-
-	for _, test := range testCases {
-		err = space.Push(test.data)
-		assert.NoError(t, err)
-	}
-
-	for _, test := range testCases {
-		var data []byte
-		data, err = space.GetValue(test.offset, len(test.data))
-		assert.NoError(t, err)
-		value := test.value
-		switch value.(type) {
-		case uint64:
-			assert.Equal(t, binary.LittleEndian.Uint64(data), value.(string))
-		case string:
-			assert.Equal(t, string(data), value.(string))
+	t.Run("Push and Get Test", func(t *testing.T) {
+		for _, test := range testCases {
+			err = space.Push(test.data)
+			assert.NoError(t, err)
 		}
-	}
 
+		for _, test := range testCases {
+			var data []byte
+			data, err = space.GetValue(test.offset, len(test.data))
+			assert.NoError(t, err)
+			value := test.value
+			switch value.(type) {
+			case uint64:
+				assert.Equal(t, binary.LittleEndian.Uint64(data), value.(uint64))
+			case string:
+				assert.Equal(t, string(data), value.(string))
+			}
+		}
+
+	})
+	t.Run("Mov and Get Test", func(t *testing.T) {
+		for _, test := range testCases {
+			err = space.MOV(test.offset, test.data)
+			assert.NoError(t, err)
+		}
+
+		for _, test := range testCases {
+			var data []byte
+			data, err = space.GetValue(test.offset, len(test.data))
+			assert.NoError(t, err)
+			value := test.value
+			switch value.(type) {
+			case uint64:
+				assert.Equal(t, binary.LittleEndian.Uint64(data), value.(uint64))
+			case string:
+				assert.Equal(t, string(data), value.(string))
+			}
+		}
+
+	})
+
+}
+
+func TestVirtualSpaceError(t *testing.T) {
+	memorySize := 512
+	memoryLimitSize := 1024
+	pageSizeErr := 256
+	space, err := memorySpace.NewAllocator(memorySize, memoryLimitSize, pageSizeErr)
+	require.NoError(t, err)
+	testValue := make([]byte, 1025)
+	err = space.MOV(0, testValue)
+	require.Error(t, err)
 }
